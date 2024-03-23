2acc986ff0d60eb36f6042829aa3f65b85af1d7b
unknown <kumaradarsh921@gmail.com>
2024-03-23 15:48:38 +0530

    Git Auto push

--- route_table.tf
+++ route_table.tf
@@ -8,6 +8,11 @@ resource "aws_route_table" "public_rt" {
     gateway_id = aws_internet_gateway.igw.id
   }
 
+  route {
+    cidr_block = var.DEFAULT_VPC_CIDR
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
   tags = {
     Name = "roboshop-${var.ENV}-public-rt"
   }
@@ -19,4 +24,17 @@ resource "aws_route_table_association" "public_subnet_rt_association" {
   count          = length(aws_subnet.public_subnet.*.id)
   subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
   route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-public-rt"
+  }
 }
