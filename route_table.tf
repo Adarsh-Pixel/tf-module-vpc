843b7b80636da2b721259eb1007648726aa49b49
unknown <kumaradarsh921@gmail.com>
2024-03-27 11:57:02 +0530

    Git Auto push

--- route_table.tf
+++ route_table.tf
@@ -44,8 +44,11 @@ resource "aws_route_table" "private_rt" {
   }
 }
 
+# Add's subnet's association with route table
+
 resource "aws_route_table_association" "private_subnet_rt_association" {
   count          = length(aws_subnet.private_subnet.*.id)
   subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
   route_table_id = aws_route_table.private_rt.id
 }
+
